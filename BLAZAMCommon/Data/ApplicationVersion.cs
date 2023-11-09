391d561c698d74e3b8b2bc6d1650fc3f7fedaffb
Chris Jacobsen Work <cjacobsen@work.com>
2023-11-09 11:39:47 -0500

    New Application Update presentation in Update settings

--- BLAZAMCommon/Data/ApplicationVersion.cs
+++ BLAZAMCommon/Data/ApplicationVersion.cs
@@ -1,8 +1,20 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 namespace BLAZAM.Common.Data
 {
+    /// <summary>
+    /// A representation of a version of the app.
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// [0.8.4].[2023.11.09.1134]
+    /// <para>
+    /// 
+    /// [AssemblyVersion][Build Number]
+    /// </para>
+    /// </remarks>
     public class ApplicationVersion : IComparable
     {
         public Version AssemblyVersion { get; private set; }
@@ -42,6 +54,47 @@ namespace BLAZAM.Common.Data
                         buildNumber += ".";
                 }
                 BuildNumber = buildNumber;
+            }
+        }
+        /// <summary>
+        /// The UTC time of release for this version
+        /// </summary>
+        /// <remarks>
+        /// This is calculated from the build number
+        /// </remarks>
+        public DateTime ReleaseDate
+        {
+            get
+            {
+                DateTime release = DateTime.MinValue;
+                var buildNumberParts = BuildNumber.Split('.');
+                string year="";
+                string month="";
+                string day="";
+                string time="";
+                for (int x = 0; x < buildNumberParts.Length; x++)
+                {
+                    switch (x)
+                    {
+                        case 0:
+                            year = buildNumberParts[x];
+                            break;
+                        case 1:
+                            month = buildNumberParts[x];
+
+                            break;
+                        case 2:
+                            day = buildNumberParts[x];
+
+                            break;
+                        case 3:
+                            time = buildNumberParts[x];
+                            time=time.Insert(2, ":");
+                            break;
+                    }
+                }
+                DateTime.TryParse((month+"/"+day+"/"+year + " "+ time+" Z"), out release);
+                return release;
             }
         }
 
