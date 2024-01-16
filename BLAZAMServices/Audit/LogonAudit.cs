ee3571088634d85291fdc4a7ee3f7371f4b0073f
Chris Jacobsen <jacobsen9026@gmail.com>
2024-01-16 15:07:39 -0500

    Audit only tracking user searches fixed

---
+++ BLAZAMServices/Audit/LogonAudit.cs
@@ -0,0 +1,42 @@
+﻿using BLAZAM.Database.Context;
+using BLAZAM.Database.Models.Audit;
+using BLAZAM.Session.Interfaces;
+
+namespace BLAZAM.Services.Audit
+{
+    public class LogonAudit : CommonAudit
+    {
+        public LogonAudit(IAppDatabaseFactory factory,
+            IApplicationUserStateService userStateService) : base(factory, userStateService)
+        {
+        }
+
+        public async Task<bool> Login(System.Security.Claims.ClaimsPrincipal user)
+        {
+            CurrentUser = UserStateService.CreateUserState(user);
+            return await Log("Login");
+        }
+        public async Task<bool> Logout() => await Log("Logout");
+
+        private async Task<bool> Log(string action)
+        {
+
+            try
+            {
+                using var context = await Factory.CreateDbContextAsync();
+                context.LogonAuditLog.Add(new LogonAuditLog
+                {
+                    Action = action,
+                    Username = CurrentUser.AuditUsername,
+                    IpAddress = CurrentUser.IPAddress?.ToString(),
+                });
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
