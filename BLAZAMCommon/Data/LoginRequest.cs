81ce3066d2fa35e0652179ea765d7477f174b933
Chris Jacobsen <jacobsen9026@gmail.com>
2024-03-23 17:12:33 -0400

    IP can be optional

--- BLAZAMCommon/Data/LoginRequest.cs
+++ BLAZAMCommon/Data/LoginRequest.cs
@@ -54,6 +54,6 @@ namespace BLAZAM.Common.Data
         /// <summary>
         /// The remote IP of the login attempt
         /// </summary>
-        public IPAddress IPAddress { get; set; }
+        public IPAddress? IPAddress { get; set; }
     }
 }
