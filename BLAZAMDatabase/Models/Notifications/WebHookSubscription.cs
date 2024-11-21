43889d0fa8d878fef382c4d67a5974c59d2b8c93
Chris Jacobsen <jacobsen9026@gmail.com>
2024-11-21 05:56:05 -0500

    Add comments

--- BLAZAMDatabase/Models/Notifications/WebHookSubscription.cs
+++ BLAZAMDatabase/Models/Notifications/WebHookSubscription.cs
@@ -16,11 +16,32 @@ namespace BLAZAM.Database.Models.Notifications
     }
     public class WebHookSubscription : RecoverableAppDbSetBase
     {
+        /// <summary>
+        /// The notification types this subscription should trigger on
+        /// </summary>
         public List<SubscriptionWebHookType> NotificationTypes { get; set; } = new();
         //public string OU { get; set; }
+
+        /// <summary>
+        /// Whether or not to ignore SSL Certificate verification when sending the webhook
+        /// </summary>
         public bool IgnoreSSLVerification { get; set; }
+
+        /// <summary>
+        /// The full URL with protocol appended
+        /// </summary>
+        /// <remarks>
+        /// eg: https://blazam.org/consumer
+        /// </remarks>
         public string URL { get; set; }
+        /// <summary>
+        /// The HTTP method to use
+        /// </summary>
         public WebHookMethod WebHookMethod { get; set; }
+
+        /// <summary>
+        /// The authorization type to use. None, basic, or bearer.
+        /// </summary>
         public WebHookAuthorization WebHookAuthorization { get;set;}
         /// <summary>
         /// The destination authorization token in encrypted form
