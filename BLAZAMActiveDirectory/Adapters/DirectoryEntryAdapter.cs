349147426d98e87f19fdc0c4314aa5ea4ada0807
Chris Jacobsen <jacobsen9026@gmail.com>
2024-11-23 18:46:17 -0500

    Fix for user creation notifiications #638

--- BLAZAMActiveDirectory/Adapters/DirectoryEntryAdapter.cs
+++ BLAZAMActiveDirectory/Adapters/DirectoryEntryAdapter.cs
@@ -19,6 +19,7 @@ using System.DirectoryServices;
 using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Web;
 using static MudBlazor.Colors;
 
@@ -305,6 +306,23 @@ namespace BLAZAM.ActiveDirectory.Adapters
         {
             get
             {
+                if (NewEntry)
+                {
+                    // Regular expression to match everything after the last slash
+                    string pattern = @"[^/]+$";
+
+                    Match match = Regex.Match(ADSPath, pattern);
+
+                    if (match.Success)
+                    {
+                        return match.Value;
+                    }
+                    else
+                    {
+                        // Handle the case where no match is found
+                        return null;
+                    }
+                }
                 return GetStringProperty("distinguishedName");
             }
             set
