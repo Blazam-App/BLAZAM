9bd2ea2a68b2dd4e17622593cd19b6d27d9b2c8f
Chris Jacobsen <jacobsen9026@gmail.com>
2025-03-01 20:56:17 -0500

    Rename system user to Blazam

--- BLAZAMSession/SystemUserState.cs
+++ BLAZAMSession/SystemUserState.cs
@@ -13,7 +13,7 @@ namespace BLAZAM.Session
         public SystemUserState(IAppDatabaseFactory factory) : base(factory)
         {
             var identity = new ClaimsIdentity();
-            identity.AddClaim(new Claim(ClaimTypes.Name, "System"));
+            identity.AddClaim(new Claim(ClaimTypes.Name, "Blazam"));
             this.User = new System.Security.Claims.ClaimsPrincipal();
             this.User.AddIdentity(identity);
         }
