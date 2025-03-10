b6251f6ff18a200f9d9396f3e8578de6f52f6b08
Chris Jacobsen <jacobsen9026@gmail.com>
2025-03-10 17:33:15 -0400

    Rule progress

--- BLAZAMDatabase/Models/AutomationRuleAction.cs
+++ BLAZAMDatabase/Models/AutomationRuleAction.cs
@@ -1,7 +1,12 @@
-﻿namespace BLAZAM.Database.Models
+﻿using BLAZAM.Database.Models.Permissions;
+
+namespace BLAZAM.Database.Models
 {
+
     public class AutomationRuleAction: RecoverableAppDbSetBase
     {
+    
+        public ActiveDirectoryObjectAction ActionType { get; set; }
         public List<AutomationRuleFieldValue> FieldValues { get; set; }
 
     }
