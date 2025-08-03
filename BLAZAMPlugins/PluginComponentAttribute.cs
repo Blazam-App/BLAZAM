9e05b94f8c329937beca58b03bccbae12b50fa4a
Chris Jacobsen <jacobsen9026@gmail.com>
2025-08-03 17:25:26 -0400

    Expand plugin integration

--- BLAZAMPlugins/PluginComponentAttribute.cs
+++ BLAZAMPlugins/PluginComponentAttribute.cs
@@ -5,18 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BLAZAM.Plugins
-{
+{   
+    /// <summary>
+    /// Determines the page type that the PluginComponent will be shown
+    /// </summary>
     public enum PageType
     {
         Settings,
+        AllEntryTypes,
         User,
         Group,
+        Contact,
         Computer,
         OU,
         Printer,
         Widget,
         Plugin,
     }
+    /// <summary>
+    /// Determines the location on the page that the PluginComponent should be shown
+    /// </summary>
     public enum PageLocation
     {
         SubHeader,
@@ -29,7 +37,10 @@ namespace BLAZAM.Plugins
         ContactInfo,
         Name,
     }
-
+    /// <summary>
+    /// This attribute provides the nececessary information for Blazam to include the attached Razor component
+    /// in the default Blazam pages. For adding entire pages use <see cref=""/>
+    /// </summary>
     public class PluginComponentAttribute : Attribute
     {
         public PageType PageType { get; }
