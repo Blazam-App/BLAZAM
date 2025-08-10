2f8365b6b23d42c9c5e2b9ef3d99507584ef657f
Chris Jacobsen <jacobsen9026@gmail.com>
2025-08-10 16:24:38 -0400

    Refactor WidgetService for improved widget handling
    Updated WidgetService to enhance widget availability logic based on user permissions. Changed private fields to readonly, introduced a helper method `AddWidgetIf` for cleaner widget addition, and moved null check for `applicationUser` to the start of the `Available` method for early return. Consolidated super admin widget addition for better clarity and maintainability.

--- BLAZAMGui/Services/WidgetService.cs
+++ BLAZAMGui/Services/WidgetService.cs
@@ -9,8 +9,8 @@ namespace BLAZAM.Gui.Services
     /// </summary>
     public class WidgetService
     {
-        private ICurrentUserStateService _currentUserStateService;
-        private IStringLocalizer<AppLocalization> AppLocalization;
+        private readonly ICurrentUserStateService _currentUserStateService;
+        private readonly IStringLocalizer<AppLocalization> AppLocalization;
 
         public WidgetService(ICurrentUserStateService currentUserStateService, IStringLocalizer<AppLocalization> appLocalization)
         {
@@ -22,57 +22,59 @@ namespace BLAZAM.Gui.Services
         {
             var applicationUser = _currentUserStateService.State;
             var widgets = new List<Widget>();
-            if (applicationUser != null)
+            if (applicationUser == null)
+                return widgets;
+
+            bool isSuperAdmin = applicationUser.IsSuperAdmin;
+
+            void AddWidgetIf(bool condition, Widget widget)
             {
-                if (applicationUser.IsSuperAdmin || applicationUser.CanUnlockUsers)
-                    widgets.Add(new LockedOutUsers() { WidgetType = DashboardWidgetType.LockedOutUsers, Title = AppLocalization[Lang.Locked_Out_Users] });
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchUsers))
-                {
-
-                    widgets.Add(new NewUsersWidget() { WidgetType = DashboardWidgetType.NewUsers, Title = AppLocalization["Users created in the last 14 days"] });
-                    widgets.Add(new ChangedPasswordsWidget() { WidgetType = DashboardWidgetType.PasswordsChanged, Title = AppLocalization["Passwords changed in the last 90 days"] });
-
-                }
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchContacts))
-                {
-
-                    widgets.Add(new NewContactsWidget() { WidgetType = DashboardWidgetType.NewContacts, Title = AppLocalization["Contacts created in the last 14 days"] });
-
-                }
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchUsers)
-                    || applicationUser.HasRole(UserRoles.SearchOUs)
-                     || applicationUser.HasRole(UserRoles.SearchGroups)
-                     || applicationUser.HasRole(UserRoles.SearchPrinters)
-                      || applicationUser.HasRole(UserRoles.SearchComputers))
-                    widgets.Add(new ChangedEntriesWidget() { WidgetType = DashboardWidgetType.ChangedEntries, Title = AppLocalization["Entries changed in the last 24 hours"] });
-
-
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchOUs))
-
-                    widgets.Add(new NewOUsWidget() { WidgetType = DashboardWidgetType.NewOus, Title = AppLocalization["OU's created in the last 14 days"] });
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchGroups))
-
-                    widgets.Add(new NewGroupsWidget() { WidgetType = DashboardWidgetType.NewGroups, Title = AppLocalization["Groups created in the last 14 days"] });
-
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchPrinters))
-
-                    widgets.Add(new NewPrintersWidget() { WidgetType = DashboardWidgetType.NewPrinters, Title = AppLocalization["Printers created in the last 14 days"] });
-
-                if (applicationUser.IsSuperAdmin || applicationUser.HasRole(UserRoles.SearchComputers))
-
-                    widgets.Add(new NewComputersWidget() { WidgetType = DashboardWidgetType.NewComputers, Title = AppLocalization["Computers created in the last 14 days"] });
-                if (applicationUser.IsSuperAdmin)
-                {
-                    widgets.Add(new DeletedEntriesWidget() { WidgetType = DashboardWidgetType.DeletedEntries, Title = AppLocalization["Entries deleted in the last 14 days"] });
-                    widgets.Add(new AppLogonsWidget() { WidgetType = DashboardWidgetType.AppLogons, Title = AppLocalization[Lang.Application_logons] });
-                    widgets.Add(new StaleUsersWidget() { WidgetType = DashboardWidgetType.StaleUsers, Title = AppLocalization[Lang.Stale_users] });
-                    widgets.Add(new StaleComputersWidget() { WidgetType = DashboardWidgetType.StaleComputers, Title = AppLocalization[Lang.Stale_computers] });
-
-                }
-
-                widgets.Add(new FavoritesWidget() { WidgetType = DashboardWidgetType.FavoriteEntries, Title = AppLocalization[Lang.Favorites] });
-
+                if (condition)
+                    widgets.Add(widget);
             }
+
+            AddWidgetIf(isSuperAdmin || applicationUser.CanUnlockUsers,
+                new LockedOutUsers() { WidgetType = DashboardWidgetType.LockedOutUsers, Title = AppLocalization[Lang.Locked_Out_Users] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchUsers),
+                new NewUsersWidget() { WidgetType = DashboardWidgetType.NewUsers, Title = AppLocalization["Users created in the last 14 days"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchUsers),
+                new ChangedPasswordsWidget() { WidgetType = DashboardWidgetType.PasswordsChanged, Title = AppLocalization["Passwords changed in the last 90 days"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchContacts),
+                new NewContactsWidget() { WidgetType = DashboardWidgetType.NewContacts, Title = AppLocalization["Contacts created in the last 14 days"] });
+
+            AddWidgetIf(isSuperAdmin
+                || applicationUser.HasRole(UserRoles.SearchUsers)
+                || applicationUser.HasRole(UserRoles.SearchOUs)
+                || applicationUser.HasRole(UserRoles.SearchGroups)
+                || applicationUser.HasRole(UserRoles.SearchPrinters)
+                || applicationUser.HasRole(UserRoles.SearchComputers),
+                new ChangedEntriesWidget() { WidgetType = DashboardWidgetType.ChangedEntries, Title = AppLocalization["Entries changed in the last 24 hours"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchOUs),
+                new NewOUsWidget() { WidgetType = DashboardWidgetType.NewOus, Title = AppLocalization["OU's created in the last 14 days"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchGroups),
+                new NewGroupsWidget() { WidgetType = DashboardWidgetType.NewGroups, Title = AppLocalization["Groups created in the last 14 days"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchPrinters),
+                new NewPrintersWidget() { WidgetType = DashboardWidgetType.NewPrinters, Title = AppLocalization["Printers created in the last 14 days"] });
+
+            AddWidgetIf(isSuperAdmin || applicationUser.HasRole(UserRoles.SearchComputers),
+                new NewComputersWidget() { WidgetType = DashboardWidgetType.NewComputers, Title = AppLocalization["Computers created in the last 14 days"] });
+
+            if (isSuperAdmin)
+            {
+                widgets.Add(new DeletedEntriesWidget() { WidgetType = DashboardWidgetType.DeletedEntries, Title = AppLocalization["Entries deleted in the last 14 days"] });
+                widgets.Add(new AppLogonsWidget() { WidgetType = DashboardWidgetType.AppLogons, Title = AppLocalization[Lang.Application_logons] });
+                widgets.Add(new StaleUsersWidget() { WidgetType = DashboardWidgetType.StaleUsers, Title = AppLocalization[Lang.Stale_users] });
+                widgets.Add(new StaleComputersWidget() { WidgetType = DashboardWidgetType.StaleComputers, Title = AppLocalization[Lang.Stale_computers] });
+            }
+
+            widgets.Add(new FavoritesWidget() { WidgetType = DashboardWidgetType.FavoriteEntries, Title = AppLocalization[Lang.Favorites] });
+
             return widgets;
         }
 
