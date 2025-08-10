ebc1aa1a4e9cfb91191e4236859097db49b79bc4
Chris Jacobsen <jacobsen9026@gmail.com>
2025-08-10 10:56:08 -0400

    Refactor DatabaseContext and improve model configuration
    Consolidated using directives in `DatabaseContextBase.cs` and changed `_pendingMigrations` and `_appliedMigrations` to instance fields. Refactored `OnModelCreating` to move entity configurations to `DatabaseHelpers.cs`, enhancing code organization. Updated `TestConnection` for better null handling and added `TestSqliteConnection`. Introduced `HandleSqlException` for centralized SQL error handling. Modified `EntityIsTracked` to accept nullable entries and added a new `ConfigureModel` method in `DatabaseHelpers.cs` to streamline model setup. Updated `GetTableName` to use the null-conditional operator for safety.

--- BLAZAMDatabase/Helpers/DatabaseHelpers.cs
+++ BLAZAMDatabase/Helpers/DatabaseHelpers.cs
@@ -1,8 +1,15 @@
 ﻿using System.Reflection;
 using BLAZAM.Common.Data;
+using BLAZAM.Database.Context;
+using BLAZAM.Database.Models;
+using BLAZAM.Database.Models.Chat;
 using BLAZAM.Database.Models.Notifications;
 using BLAZAM.Database.Models.Permissions;
+using BLAZAM.Database.Models.Rules;
+using BLAZAM.Database.Models.Templates;
 using BLAZAM.Database.Models.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BLAZAM.Helpers
 {
@@ -116,6 +123,297 @@ namespace BLAZAM.Helpers
                 .Select(fi => (TProperty?)fi.GetValue(null)) // Get the static value (pass null for static fields)
                 .Where(value => value != null) // Ensure the value isn't null
                 .ToList();
+        }
+
+        public static void ConfigureModel(ModelBuilder modelBuilder, DatabaseContextBase context)
+        {
+            AddActiveDirectoryFieldData(modelBuilder);
+            AddCustomActiveDirectoryFieldConfig(modelBuilder);
+            AddAccessLevelConfig(modelBuilder);
+            AddFieldAccessLevelData(modelBuilder);
+            AddFieldAccessMappingConfig(modelBuilder);
+            AddObjectAccessLevelData(modelBuilder);
+            AddObjectAccessMappingConfig(modelBuilder);
+            AddDirectoryTemplateConfig(modelBuilder);
+            AddAutomationRuleConfig(modelBuilder);
+            AddAutomationRuleActionConfig(modelBuilder);
+            AddAutomationRuleOrFilterConfig(modelBuilder);
+            AddAutomationRuleAndFilterConfig(modelBuilder);
+            AddAutomationRuleActionFieldValueConfig(modelBuilder);
+            AddDirectoryTemplateFieldValueConfig(modelBuilder);
+            AddObjectActionData(modelBuilder);
+            AddActionAccessMappingConfig(modelBuilder);
+            AddPermissionMappingConfig(modelBuilder);
+            AddAppSettingsConfig(modelBuilder, context);
+            AddADSettingsConfig(modelBuilder, context);
+            AddAuthenticationSettingsConfig(modelBuilder, context);
+            AddEmailSettingsConfig(modelBuilder, context);
+            AddAppUserConfig(modelBuilder);
+            AddUserNotificationConfig(modelBuilder);
+            AddPermissionDelegateConfig(modelBuilder);
+            AddChatRoomConfig(modelBuilder);
+            AddChatMessageConfig(modelBuilder);
+            AddNotificationSubscriptionConfig(modelBuilder);
+            AddUnreadChatMessageConfig(modelBuilder);
+        }
+
+        internal static void AddActiveDirectoryFieldData(ModelBuilder modelBuilder)
+        {
+            List<ActiveDirectoryField> activeDirectoryFields = typeof(ActiveDirectoryFields).GetStaticProperties<ActiveDirectoryField>();
+            modelBuilder.Entity<ActiveDirectoryField>().HasData(activeDirectoryFields);
+        }
+
+        internal static void AddCustomActiveDirectoryFieldConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomActiveDirectoryField>().HasMany(x => x.ObjectTypes);
+            modelBuilder.Entity<CustomActiveDirectoryField>().Navigation(x => x.ObjectTypes).AutoInclude();
+        }
+
+        internal static void AddAccessLevelConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccessLevel>(entity =>
+            {
+                entity.HasData(new AccessLevel { Id = 1, Name = "Deny All" });
+                entity.Navigation(e => e.ObjectMap).AutoInclude();
+                entity.Navigation(e => e.FieldMap).AutoInclude();
+                entity.Navigation(e => e.ActionMap).AutoInclude();
+            });
+        }
+
+        internal static void AddFieldAccessLevelData(ModelBuilder modelBuilder)
+        {
+            List<FieldAccessLevel> fieldAccessLevels = typeof(FieldAccessLevels).GetStaticProperties<FieldAccessLevel>();
+            modelBuilder.Entity<FieldAccessLevel>().HasData(fieldAccessLevels);
+        }
+
+        internal static void AddFieldAccessMappingConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FieldAccessMapping>(entity =>
+            {
+                entity.Navigation(e => e.CustomField).AutoInclude();
+                entity.Navigation(e => e.Field).AutoInclude();
+                entity.Navigation(e => e.FieldAccessLevel).AutoInclude();
+            });
+        }
+
+        internal static void AddObjectAccessLevelData(ModelBuilder modelBuilder)
+        {
+            List<ObjectAccessLevel> objectAccessLevels = typeof(ObjectAccessLevels).GetStaticProperties<ObjectAccessLevel>();
+            modelBuilder.Entity<ObjectAccessLevel>(entity =>
+            {
+                entity.HasData(objectAccessLevels);
+            });
+        }
+
+        internal static void AddObjectAccessMappingConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ObjectAccessMapping>(entity =>
+            {
+                entity.Navigation(e => e.ObjectAccessLevel).AutoInclude();
+            });
+        }
+
+        internal static void AddDirectoryTemplateConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DirectoryTemplate>(entity =>
+            {
+                entity.Navigation(e => e.FieldValues).AutoInclude();
+                entity.Navigation(e => e.AssignedGroupSids).AutoInclude();
+            });
+        }
+
+        internal static void AddAutomationRuleConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AutomationRule>(entity =>
+            {
+                entity.Navigation(e => e.Actions).AutoInclude();
+                entity.Navigation(e => e.Filters).AutoInclude();
+            });
+        }
+
+        internal static void AddAutomationRuleActionConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AutomationRuleAction>(entity =>
+            {
+                entity.Navigation(e => e.GroupSids).AutoInclude();
+                entity.Navigation(e => e.FieldValues).AutoInclude();
+            });
+        }
+
+        internal static void AddAutomationRuleOrFilterConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AutomationRuleOrFilter>(entity =>
+            {
+                entity.Navigation(e => e.AndFilters).AutoInclude();
+            });
+        }
+
+        internal static void AddAutomationRuleAndFilterConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AutomationRuleAndFilter>(entity =>
+            {
+                entity.Navigation(e => e.CustomField).AutoInclude();
+                entity.Navigation(e => e.Field).AutoInclude();
+                entity.Property(e => e.TimeFrame)
+                    .HasConversion(new ValueConverter<TimeSpan?, long?>(
+                        v => v.HasValue ? v.Value.Ticks : (long?)null,
+                        v => v.HasValue ? TimeSpan.FromTicks(v.Value) : (TimeSpan?)null
+                    ));
+            });
+        }
+
+        internal static void AddAutomationRuleActionFieldValueConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AutomationRuleActionFieldValue>(entity =>
+            {
+                entity.Navigation(e => e.Field).AutoInclude();
+                entity.Navigation(e => e.CustomField).AutoInclude();
+            });
+        }
+
+        internal static void AddDirectoryTemplateFieldValueConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DirectoryTemplateFieldValue>(entity =>
+            {
+                entity.Navigation(e => e.Field).AutoInclude();
+                entity.Navigation(e => e.CustomField).AutoInclude();
+            });
+        }
+
+        internal static void AddObjectActionData(ModelBuilder modelBuilder)
+        {
+            List<ObjectAction> objectActions = typeof(ObjectActions).GetStaticProperties<ObjectAction>();
+            modelBuilder.Entity<ObjectAction>().HasData(objectActions);
+        }
+
+        internal static void AddActionAccessMappingConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ActionAccessMapping>(entity =>
+            {
+                entity.Navigation(e => e.ObjectAction).AutoInclude();
+            });
+        }
+
+        internal static void AddPermissionMappingConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PermissionMapping>(entity =>
+            {
+                entity.Navigation(e => e.AccessLevels).AutoInclude();
+            });
+        }
+
+        internal static void AddAppSettingsConfig(ModelBuilder modelBuilder, DatabaseContextBase context)
+        {
+            modelBuilder.Entity<AppSettings>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                if (context.Database.IsMySql())
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "Id = 1"));
+                else
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "[Id] = 1"));
+            });
+        }
+
+        internal static void AddADSettingsConfig(ModelBuilder modelBuilder, DatabaseContextBase context)
+        {
+            modelBuilder.Entity<ADSettings>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                if (context.Database.IsMySql())
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "Id = 1"));
+                else
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "[Id] = 1"));
+            });
+        }
+
+        internal static void AddAuthenticationSettingsConfig(ModelBuilder modelBuilder, DatabaseContextBase context)
+        {
+            modelBuilder.Entity<AuthenticationSettings>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                if (context.Database.IsMySql())
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "Id = 1"));
+                else
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "[Id] = 1"));
+                entity.HasData(new AuthenticationSettings
+                {
+                    Id = 1,
+                    AdminPassword = "password"
+                });
+            });
+        }
+
+        internal static void AddEmailSettingsConfig(ModelBuilder modelBuilder, DatabaseContextBase context)
+        {
+            modelBuilder.Entity<EmailSettings>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                if (context.Database.IsMySql())
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "Id = 1"));
+                else
+                    entity.ToTable(t => t.HasCheckConstraint("CK_Table_Column", "[Id] = 1"));
+            });
+        }
+
+        internal static void AddAppUserConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>(entity =>
+            {
+                entity.HasIndex(e => e.UserGUID).IsUnique();
+                entity.Navigation(e => e.ReadNewsItems).AutoInclude();
+                entity.Navigation(e => e.FavoriteEntries).AutoInclude();
+                entity.Navigation(e => e.DashboardWidgets).AutoInclude();
+            });
+        }
+
+        internal static void AddUserNotificationConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserNotification>(entity =>
+            {
+                entity.Navigation(e => e.Notification).AutoInclude();
+            });
+        }
+
+        internal static void AddPermissionDelegateConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PermissionDelegate>(entity =>
+            {
+                entity.HasIndex(e => e.DelegateSid).IsUnique();
+            });
+        }
+
+        internal static void AddChatRoomConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatRoom>(entity =>
+            {
+                entity.HasMany(e => e.Members).WithMany();
+                entity.Navigation(e => e.Messages).AutoInclude();
+                entity.Navigation(e => e.Members).AutoInclude();
+            });
+        }
+
+        internal static void AddChatMessageConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatMessage>(entity =>
+            {
+                entity.Navigation(e => e.User).AutoInclude();
+            });
+        }
+
+        internal static void AddNotificationSubscriptionConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NotificationSubscription>(entity =>
+            {
+                entity.Navigation(e => e.NotificationTypes).AutoInclude();
+            });
+        }
+
+        internal static void AddUnreadChatMessageConfig(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UnreadChatMessage>(entity =>
+            {
+                entity.Navigation(e => e.ChatMessage).AutoInclude();
+            });
         }
     }
 }
