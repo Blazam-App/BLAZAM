091b9853a6015b2cae7db1f1535d93285bab6d9d
jacobsen9026 <admin@blazam.org>
2025-08-11 11:36:53 -0400

    Working for directory navigation, except when clicking a file, then up one directory button is disabled, because the parent directory Exists property returns false, but it exists.

--- BLAZAMGui/UI/Computers/ComputerFileSystemBrowser.razor.cs
+++ BLAZAMGui/UI/Computers/ComputerFileSystemBrowser.razor.cs
@@ -7,8 +7,8 @@ namespace BLAZAM.Gui.UI.Computers
     {
         [Inject]
         private IJSRuntime JSRuntime { get; set; }
-        private MudDataGrid<FileSystemBase>? _dataGrid;
-        private IEnumerable<FileSystemBase> items { get; set; } = new List<FileSystemBase>();
+        private MudDataGrid<IFileSystemObject>? _dataGrid;
+        private IEnumerable<IFileSystemObject> items { get; set; } = new List<IFileSystemObject>();
         private SystemDirectory? currentPath { get; set; }
         private List<IADComputerDrive> _drives = [];
         private IADComputer? _computer;
@@ -18,6 +18,17 @@ namespace BLAZAM.Gui.UI.Computers
             {
 
                 _computer = Computer;
+             
+                    LoadDrives(_computer.IsOnline == true);
+                
+                _computer.OnOnlineChanged += LoadDrives;
+            }
+        }
+
+        private void LoadDrives(bool online)
+        {
+            if (online && _computer!=null)
+            {
                 _drives = _computer.GetDrives();
                 if (_drives.Count > 0)
                 {
@@ -27,30 +38,53 @@ namespace BLAZAM.Gui.UI.Computers
             }
         }
 
+        /// <summary>
+        /// Loads the items in the current directory.
+        /// </summary>
         private async void LoadItems()
         {
-            if (currentPath != null)
+            if (currentPath != null && _computer!=null && _computer.IsOnline==true)
             {
                 await _computer.Directory.Impersonation.RunAsync(() =>
                 {
-                    items = currentPath.Files;
-                    return true;
-                });
-                await _computer.Directory.Impersonation.RunAsync(() =>
-                {
-                    var directories = currentPath.SubDirectories;
-                    directories.ForEach(dir =>
+                    items = currentPath.SubDirectories;
+                    var files = currentPath.Files;
+                    files.ForEach(file =>
                     {
-                        items = items.Append(dir);
+                        items = items.Append(file);
                     });
                     InvokeAsync(StateHasChanged);
                     return true;
                 });
             }
         }
+        /// <summary>
+        /// Handles the row click event in the data grid.
+        /// </summary>
+        /// <param name="args"></param>
+        private async void RowClicked(DataGridRowClickEventArgs<IFileSystemObject>args)
+        {
+            var item = args.Item;
+            if(item is SystemFile file)
+            {
+                //TODO: Check if the file is a valid file type for download
+            }
+            else if (item is SystemDirectory directory)
+            {
+                currentPath = directory;
+                LoadItems();
+            }
+        }
 
-
-
+        private void GoUpOneDirectory()
+        {
+            if (currentPath?.ParentDirectory != null)
+            {
+                currentPath = currentPath.ParentDirectory;
+                LoadItems();
+            }
+        }
+        private bool CanGoUp => (currentPath == null || currentPath.ParentDirectory == null || !currentPath.ParentDirectory.Exists);
 
         private async void Download(SystemFile item)
         {
