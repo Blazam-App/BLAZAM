0db2aa2ca8a520c632982ec9ebff925129dc63b2
Chris Jacobsen <jacobsen9026@gmail.com>
2025-08-18 16:56:13 -0400

    Move EnumSchemaFilter to BLAZAM.Data namespace

--- BLAZAM/Data/EnumSchemaFilter.cs
+++ BLAZAM/Data/EnumSchemaFilter.cs
@@ -3,20 +3,31 @@ using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-public class EnumSchemaFilter : ISchemaFilter
+namespace BLAZAM.Data
 {
-    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    public class EnumSchemaFilter : ISchemaFilter
     {
-        if (context.Type.IsEnum)
+        /// <summary>
+        /// Modifies the provided <see cref="OpenApiSchema"/> to replace numeric enum values with their string names.
+        /// </summary>
+        /// <remarks>This method is specifically designed to handle enum types. If the type in the
+        /// <paramref name="context"/> is an enum, the numeric values in the <paramref name="schema"/> are replaced with
+        /// the corresponding string names of the enum fields.</remarks>
+        /// <param name="schema">The OpenAPI schema to be modified.</param>
+        /// <param name="context">The context containing metadata about the schema, including the type being processed.</param>
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
         {
-            // Replace the numeric values in schema.Enum with string names
-            schema.Enum.Clear();
-            foreach (var field in context.Type.GetFields(BindingFlags.Public | BindingFlags.Static))
+            if (context.Type.IsEnum)
             {
-                schema.Enum.Add(new OpenApiString(field.Name));
+                // Replace the numeric values in schema.Enum with string names
+                schema.Enum.Clear();
+                foreach (var field in context.Type.GetFields(BindingFlags.Public | BindingFlags.Static))
+                {
+                    schema.Enum.Add(new OpenApiString(field.Name));
+                }
+
+
             }
-
-
         }
     }
 }
