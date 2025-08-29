55a1d2f8cb7a203964fbd51fe5edfae790f3cdc5
Chris Jacobsen <jacobsen9026@gmail.com>
2025-08-29 13:06:02 -0400

    Removed the `#nullable disable warnings` directive.

---
+++ BLAZAMGui/UI/Settings/ValidatedForm.cs
@@ -0,0 +1,62 @@
+using MudBlazor;
+
+namespace BLAZAM.Gui.UI.Settings
+{
+    public abstract class ValidatedForm : DatabaseComponentBase
+    {
+        protected bool SaveDisabled { get; set; }
+
+        protected MudForm? Form;
+        protected bool IsValid
+        {
+            get;
+            set;
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                Form?.Validate();
+            }
+        }
+
+        protected virtual async Task Save()
+        {
+            if (Context != null)
+            {
+                try
+                {
+                    if (ApplicationInfo.InDemoMode && ApplicationInfo.InstallationCompleted && UserStateService.CurrentUsername.Equals("demo", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SnackBarService.Warning("Settings changes are not allowed in the demo");
+                        return;
+
+                    }
+                    var results = await Context.SaveChangesAsync();
+                    if (results > 0)
+                    {
+                        SnackBarService.Success("Settings have been saved");
+                    }
+                    else
+                    {
+                        SnackBarService.Error(AppHelpLocalization[HelpLang.Unexpected_Error_Occurred]);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    SnackBarService.Error(ex.Message);
+
+                }
+                await InvokeAsync(StateHasChanged);
+            }
+            else
+            {
+                SnackBarService.Success("Database not available");
+
+            }
+        }
+
+    }
+}
