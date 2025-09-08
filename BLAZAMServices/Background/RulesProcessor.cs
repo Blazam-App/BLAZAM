d63e690fa28f1974745745468cd64e8a83aff2af
Chris Jacobsen <jacobsen9026@gmail.com>
2025-09-08 18:45:50 -0400

    Also check for DOMAIN\username

--- BLAZAMServices/Background/RulesProcessor.cs
+++ BLAZAMServices/Background/RulesProcessor.cs
@@ -194,7 +194,8 @@ namespace BLAZAM.Services.Background
             using var context = dbFactory.CreateDbContext();
             if (entry is IADUser user &&
                 (user.SAMAccountName?.Equals(context.ActiveDirectorySettings.First()?.Username, StringComparison.InvariantCultureIgnoreCase) == true
-                || user.UserPrincipalName?.Equals(context.ActiveDirectorySettings.First()?.Username, StringComparison.InvariantCultureIgnoreCase) == true))
+                || user.UserPrincipalName?.Equals(context.ActiveDirectorySettings.First()?.Username, StringComparison.InvariantCultureIgnoreCase) == true
+                || (context.ActiveDirectorySettings.First()?.Username.EndsWith("\\" + user.SAMAccountName, StringComparison.InvariantCultureIgnoreCase) == true))
             {
                 Loggers.RulesLogger.Information("Preventing rule execution on application identity.");
                 return true;
