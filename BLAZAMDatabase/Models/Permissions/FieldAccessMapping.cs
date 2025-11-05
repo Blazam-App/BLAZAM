746b4952bb005a8314b0d4c4d88f32164a4f2ad1
jacobsen9026 <jacobsen9026@gmail.com>
2025-11-04 20:08:16 -0500

    Refactor FieldAccessMapping class structure
    Updated FieldAccessMapping to inherit from ActiveDirectoryFieldDbSet, replacing AppDbSetBase. Removed unused properties (`Field`, `FieldId`, `CustomField`, `CustomFieldId`) to simplify the class and align with the updated data model. Retained essential properties for functionality.

--- BLAZAMDatabase/Models/Permissions/FieldAccessMapping.cs
+++ BLAZAMDatabase/Models/Permissions/FieldAccessMapping.cs
@@ -3,16 +3,10 @@
 
 namespace BLAZAM.Database.Models.Permissions
 {
-    public class FieldAccessMapping : AppDbSetBase
+    public class FieldAccessMapping : ActiveDirectoryFieldDbSet
     {
         public ActiveDirectoryObjectType ObjectType { get; set; }
 
-        public ActiveDirectoryField? Field { get; set; }
-
-        public int? FieldId { get; set; }
-        public CustomActiveDirectoryField? CustomField { get; set; }
-
-        public int? CustomFieldId { get; set; }
         public int FieldAccessLevelId { get; set; }
         public FieldAccessLevel FieldAccessLevel { get; set; }
         public List<AccessLevel> AccessLevels { get; set; }
