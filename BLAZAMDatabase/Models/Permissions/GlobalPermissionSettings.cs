1cb443210799b5120786686844497def85cc8407
jacobsen9026 <jacobsen9026@gmail.com>
2025-12-13 12:57:39 -0500

    Rename AllowAccessRequest to AllowActionAccessRequest
    Renamed the AllowAccessRequest property to AllowActionAccessRequest in GlobalPermissionSettings and updated all related references in the UI and logic. Updated bindings and conditional checks in GlobalPermissions.razor and DirectoryEntryViewHeader.razor.cs. Bumped project version to 2025.12.13.1756.

--- BLAZAMDatabase/Models/Permissions/GlobalPermissionSettings.cs
+++ BLAZAMDatabase/Models/Permissions/GlobalPermissionSettings.cs
@@ -4,7 +4,7 @@
     {
         public bool AllowSelfModification { get; set; }
 
-        public bool AllowAccessRequest { get; set; }
+        public bool AllowActionAccessRequest { get; set; }
 
     }
 }
